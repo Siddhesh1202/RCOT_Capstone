427a6883aa67310cfe4ed72c8dd5efa3889b3e42
Siddhesh Abhijeet Dhonde <sd1386@rit.edu>
2025-04-14 14:25:36 -0400

    testing code

--- tester_noise.py
+++ tester_noise.py
@@ -33,7 +33,7 @@ parser.add_argument("--noise_sigma", default=50, type=int, help="gpu ids (defaul
 # parser.add_argument("--tarset", default="./datasets/Denoising/CBSD68/original/", type=str, help="target data")
 parser.add_argument("--save", default="/kaggle/working/save/", type=str, help="savepath, Default: results")
 parser.add_argument("--savetar", default="/kaggle/input/kodak24/Kodak24/original/", type=str, help="savepath, Default: targets")
-parser.add_argument("--saveres", default="/kaggle/working/savres/", type=str, help="savepath, Default: residual")
+parser.add_argument("--saveres", default="/kaggle/working/savres", type=str, help="savepath, Default: residual")
 parser.add_argument("--degset", default="/kaggle/input/kodak24/Kodak24/degraded/", type=str, help="degraded data")
 parser.add_argument("--tarset", default="/kaggle/input/kodak24/Kodak24/degraded/", type=str, help="target data")
 
